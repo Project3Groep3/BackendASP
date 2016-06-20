f1e93ff607c76018d73996202360cf6e93d27e44
Mikedamen9 <mike.damen@edu-kw1c.nl>
2016-06-20 13:42:40 +0200

    Hell

--- push.bat
+++ push.bat
@@ -2,8 +2,9 @@
 
 set /p addMsg=Voeg een Commit bericht toe: 
 start /b cmd.exe /c git add -A
-timeout 5
+timeout 3
 start /b cmd.exe /c git commit -m "%addMsg%"
-timeout 5
+timeout 3
 start /b cmd.exe /c git push
+timeout 3
 pause
