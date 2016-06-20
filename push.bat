b55e4fe21c014d651f3bf6feaaddcddd0342022a
Mikedamen9 <mike.damen@edu-kw1c.nl>
2016-06-20 13:37:12 +0200

    l

--- push.bat
+++ push.bat
@@ -2,10 +2,9 @@
 
 set /p addMsg=Voeg een Commit bericht toe: 
 start /b cmd.exe /c git add -A
+echo Slaap tijd
 sleep 3
 start /b cmd.exe /c git commit -m "%addMsg%"
 sleep 5
 start /b cmd.exe /c git push
-
-
 pause
